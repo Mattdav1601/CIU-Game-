8002f7012e283c61027574be38fea82710031477
Kirsty Tschirpig <1004120@student.sae.edu.au>
2017-04-12 00:14:37 +1000

    Stage 1 - 3 coding... complete? It's code and it works I don't care. You can pick a gender woo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameManagement
+{
+    public class GameManager : MonoBehaviour
+    {
+        public float mGender;
+        public float fGender;
+
+        public float mObjects;
+        public float fObjects;
+        public int objectCount = 3;
+
+        public int hobbyCount = 1;
+        public int jobCount = 1;
+
+        public bool male;
+        public bool female;
+        public bool neutral;
+
+        public static GameManager instance = null;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            mGender = 0;
+            fGender = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (objectCount <= 0 && hobbyCount <= 0 && jobCount <= 0)
+            {
+                if (mGender > fGender)
+                {
+                    male = true;
+                }
+
+                if (mGender < fGender)
+                {
+                    female = true;
+                }
+
+                if (mGender == fGender)
+                {
+                    neutral = true;
+                }
+            }
+        }
+    }
+}
