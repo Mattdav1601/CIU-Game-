24e4263c12f334239cef73eeb9efe92b74092bdb
Matthew Davies <mdavies4@sae.au>
2017-04-27 17:54:47 +1000

    So I added text real hacky like... And added a main menu but for some reason I couldnt get the play button or the others to work... Don't know why.

---
+++ Assets/Scripts/TextScript(OhBoy)/Startingtext.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Startingtext: MonoBehaviour
+{
+    public GameObject StartRoom;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void textDisplay()
+    {
+        StartRoom.SetActive(true);
+
+    }
+
+        public void textStop()
+    {
+        StartRoom.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textDisplay();
+        }
+    }
+
+
+        private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            textStop();
+        }
+    }
+}
